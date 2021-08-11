f7e6bc740f97f5f2187bdbb5b742048d8a2ee574
Dmytro Melnichenko <dmytro.melnichenko@gmail.com>
2021-08-11 13:09:00 +0300

    added the ability to switch from "/show-error" -> "/", added withRouter functionality.

--- src/containers/ErrorBoundary/ErrorBoundary.js
+++ src/containers/ErrorBoundary/ErrorBoundary.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react'
 import Error from '../../pages/Error/Error';
+import {withRouter} from 'react-router-dom'
 
-export default class ErrorBoundary extends Component {
+class ErrorBoundary extends Component {
   state = {
     hasError: false
   }
   componentDidCatch(error, info){
     this.setState({hasError: true})
+  }
+  componentDidUpdate(prevProps){
+    const {location} = this.props;
+    //debugger;
+    if (location !== prevProps.location){
+      this.setState({hasError: false})
+    }
   }
   render() {
     const {hasError} = this.state;
@@ -19,3 +27,5 @@ export default class ErrorBoundary extends Component {
     )
   }
 }
+
+export default withRouter(ErrorBoundary);

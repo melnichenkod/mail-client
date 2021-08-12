b5bc5cbfe7d263620e7c6d4a7c524f312a46da18
Dmytro Melnichenko <dmytro.melnichenko@gmail.com>
2021-08-12 20:04:02 +0300

    created refs login, password, added listener signIn in Login component

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -2,19 +2,31 @@ import React, { Component } from 'react';
 import './Login.scss'
 
 export default class Login extends Component {
+  constructor(props){
+    super(props);
+    this.login = React.createRef(null);
+    this.password = React.createRef(null)
+  }
+  signIn = () => {
+    console.log('login', this.login.current.value);
+    console.log('password', this.password.current.value);
+  }
   render() {
+    console.log('Login', this.login);
+    console.log('Password', this.password);
+    
     return (
       <div className='login'>
         <h2>Please log in</h2>
         <div className='login__form'>
           <div>
-            <input type='text' placeholder='User name'/>
+            <input type='text' placeholder='User name' ref={this.login }/>
           </div>
           <div>
-            <input type='password' placeholder='Password'/>
+            <input type='password' placeholder='Password' ref={this.password}/>
           </div>
           <div>
-            <button>
+            <button onClick={this.signIn}>
               Sign in
             </button>
           </div>

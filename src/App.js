9f0e41434fcf0211958f8652a5daa273459b1abb
Dmytro Melnichenko <dmytro.melnichenko@gmail.com>
2021-08-13 23:21:15 +0300

    changed class component App.js to function component.

--- src/App.js
+++ src/App.js
@@ -1,34 +1,25 @@
-import React, { Component } from 'react'
+import React, {useState, useEffect } from 'react'
 import axios from 'axios'
 import Loading from './components/Loading/Loading'
 import Header from './containers/Header/Header'
 import AppRoutes from './routes/AppRoutes'
-class App extends Component {
-  state = {
-    emails: [],
-    loading: true,
-    error: null,
-    user: null
-  }
-  componentDidMount(){
-    axios('/emails.json')
+const App = () => {
+    const [emails, setEmails] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+    const [user, setUser] = useState(null);
+    /* const signIn = (user) => {
+      setUser(user)
+    }; */
+    useEffect(()=> {
+      axios('/emails.json')
       .then(result => {
         setTimeout(()=> {
-          this.setState({
-            loading: false,
-            emails: result.data
-          })
+          setEmails(result.data);
+          setLoading(false)
         }, 3000)
       })
-  };
-  signIn = (user) => {
-    this.setState({
-      user: user
-    });
-  };
-
-  render(){
-    const {emails, loading, user} = this.state;
+    }, [])
     if (loading){
       return <Loading />
     }
@@ -37,10 +28,9 @@ class App extends Component {
     return (
       <div className="App">
         <Header user={user} />
-        <AppRoutes emails={emails} signIn={this.signIn} user={user}/>
+        <AppRoutes emails={emails} signIn={setUser} user={user}/>
       </div>
     )
-  }
 }
 
 export default App;
